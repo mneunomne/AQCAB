842784cce219f149aba9580ef822a25bac9b807c
Alberto Harres <alberto.harres@gmail.com>
2023-05-21 21:00:36 +0200

    add connections content

---
+++ pages/connections/_connections.vue
@@ -0,0 +1,20 @@
+<template>
+  <article v-if="node" class="main article">
+    <h1 class="article-title">{{ node.title }}</h1>
+    <p class="mt-4">{{ node_name_en }}</p>
+    <p class="mt-4">{{ node.name_be }}</p>
+    <div class="block mt-8 mb-4" v-html="$md.render(node.content_en)" />
+    <div class="block mt-8 mb-4" v-html="$md.render(node.content_be)" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { node: payload }
+    else
+      return {
+        node: await require(`~/assets/content/connections/${params.node}.json`),
+      }
+  },
+}
+</script>
